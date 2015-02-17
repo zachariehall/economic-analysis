9e6dae4e8782c9e1c292ccf31d7fa7612caf9027
zachariehall <zacharie.hall@gmail.com>
2015-02-17 11:29:14 -0500

    cleaned up TSP Data

--- TSP_Data.R
+++ TSP_Data.R
@@ -12,17 +12,33 @@ TSPSharePrices <- read.csv(file=TSP_DATA_FILE,head=TRUE,sep=",")
 #TSPSharePrices$date <- as.POSIXlt(TSPSharePrices$date, "%Y-%m-%d")
 TSPSharePrices$date <- as.Date(TSPSharePrices$date, "%Y-%m-%d")
 
-plot(x=TSPSharePrices$date, y=TSPSharePrices$S.Fund, ylim=c(0,1.1*max(TSPSharePrices$S.Fund)),
-      type="l", ann=FALSE, xlab='Date', col='black')
-points(x=TSPSharePrices$date, y=TSPSharePrices$C.Fund, col='blue', type='l', lwd=2)
-points(x=TSPSharePrices$date, y=TSPSharePrices$G.Fund, col='purple', type='l', lwd=2)
-points(x=TSPSharePrices$date, y=TSPSharePrices$L.2050, col='red', type='l', lwd=2)
-
-legend("topleft", 40, c("S-Fund","C-Fund", "G-Fund", "L-2050"), lty=c(1,1,1,1), lwd=c(2,2,2,2), col=c("black","blue","purple", "red"))
-title(ylab="Share Prices ($)")
-
-datesOfInterest <- as.Date(seq(as.Date("2010-01-01"), length=24, by="1 month") - 1, "%Y-%m-%d")
-
+tspSharePricePlot <- function(periodStartDate, periodEndDate, plotTitle) {
+  ymax = 1.1*max(TSPSharePrices$S.Fund)
+  
+  dev.off()
+  layout(rbind(1,2), heights=c(7,1)) 
+  plot(x=TSPSharePrices$date, y=TSPSharePrices$S.Fund, ylim=c(0,ymax),
+       xlim=c(periodStartDate,periodEndDate), type="l", ann=FALSE, xlab='Date', col='black')
+  
+  points(x=TSPSharePrices$date, y=TSPSharePrices$C.Fund, col='blue', type='l', lwd=2)
+  points(x=TSPSharePrices$date, y=TSPSharePrices$G.Fund, col='purple', type='l', lwd=2)
+  points(x=TSPSharePrices$date, y=TSPSharePrices$I.Fund, col='orange', type='l', lwd=2)
+  points(x=TSPSharePrices$date, y=TSPSharePrices$L.2050, col='red', type='l', lwd=2)
+  
+  abline(h=seq(0,ymax, 5), col='grey', lwd=0.5)
+  
+  title(ylab="Share Prices ($)", main = plotTitle)
+  
+  par(mar=c(0, 0, 0, 0))
+  
+  plot.new()
+  
+  legend("center", "gropus", c("S-Fund","C-Fund", "G-Fund", "I-Fund", "L-2050"), lty=c(1,1,1,1), lwd=c(2,2,2,2), 
+         col=c("black","blue","purple", "orange", "red"), ncol = 5, bty="n")
+  
+  datesOfInterest <- as.Date(seq(as.Date("2010-01-01"), length=24, by="1 month") - 1, "%Y-%m-%d")
+  
+}
 
 ####### GENERATING TSP RETURNS ######
 
@@ -41,7 +57,7 @@ minus180 <- endTime - 180
 minus365 <- endTime - 365
 
 
-tspPeriodReturn <- function(periodEndDate, periodStartDate) {
+tspPeriodReturn <- function(periodStartDate, periodEndDate) {
   ##### Calculatiing returns based on arbretary dates provided
   # Row that is closest to but not after the date provided  
   periodEndShares <- TSPSharePrices[which.max(TSPSharePrices$date <= as.Date(periodEndDate)), ]
@@ -64,8 +80,11 @@ tspPeriodReturn <- function(periodEndDate, periodStartDate) {
 }
 
 
-minus30Returns <- tspPeriodReturn(endTime, minus30)
-minus60Returns <- tspPeriodReturn(endTime, minus60)
-minus90Returns <- tspPeriodReturn(endTime, minus90)
-minus180Returns <- tspPeriodReturn(endTime, minus180)
-minus365Returns <- tspPeriodReturn(endTime, minus365)
+minus30Returns <- tspPeriodReturn(minus30, endTime)
+minus60Returns <- tspPeriodReturn(minus60, endTime)
+minus90Returns <- tspPeriodReturn(minus90, endTime)
+minus180Returns <- tspPeriodReturn(minus180, endTime)
+minus365Returns <- tspPeriodReturn(minus365, endTime)
+
+tspSharePricePlot(minus365, endTime, "365 Day Prices")
+#tspSharePricePlot(minus90, endTime, "90 Day Prices")
